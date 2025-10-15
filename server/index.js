25b6edc6402b00a5cd1d2c09a47420101b1dca2b
Chandranil Adhikary <chandraniladhikary16dec2003@gmail.com>
2025-10-15 22:13:23 +0530

    made changes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
+
 const app = require('./src/app');
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-});
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
